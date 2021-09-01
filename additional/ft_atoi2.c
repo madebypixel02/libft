1547cec5a4ef1fe59299695ce7ddc8f0654fb767
madebypixel02 <madebypixel02@gmail.com>
2021-09-01 19:40:14 +0200

    Fixed accepting just a sign (+ or -) as valid number

--- additional/ft_atoi2.c
+++ additional/ft_atoi2.c
@@ -6,7 +6,7 @@
 /*   By: aperez-b <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/08/29 20:52:48 by aperez-b          #+#    #+#             */
-/*   Updated: 2021/08/29 21:45:38 by aperez-b         ###   ########.fr       */
+/*   Updated: 2021/09/01 19:39:43 by aperez-b         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,7 +23,7 @@ int	ft_atoi2(const char *nptr, int *nbr)
 	n = 0;
 	if (*nptr == '-')
 		sign = -sign;
-	if (*nptr == '-' || *nptr == '+')
+	if ((*nptr == '-' || *nptr == '+') && *(nptr + 1))
 		nptr++;
 	while (*nptr >= '0' && *nptr <= '9')
 	{
