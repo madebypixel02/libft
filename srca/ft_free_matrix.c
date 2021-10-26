8cbb66d772ca036dc01d539560ed04be9da809aa
madebypixel02 <madebypixel02@gmail.com>
2021-10-26 20:58:25 +0200

    Fixed issues with ft_free_matrix

--- srca/ft_free_matrix.c
+++ srca/ft_free_matrix.c
@@ -17,11 +17,12 @@ void	ft_free_matrix(char ***m)
 	int	i;
 
 	i = 0;
-	while (m[0][i])
+	while (m && m[0] && m[0][i])
 	{
 		free(m[0][i]);
 		i++;
 	}
-	free(m[0]);
+	if (m)
+		free(m[0]);
 	m = NULL;
 }
