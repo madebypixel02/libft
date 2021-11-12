2d04ba6f8054012e68edcbd8156c0a16b5458bde
madebypixel02 <madebypixel02@gmail.com>
2021-11-12 08:03:19 +0100

    Improved ft_matrix_replace_in to remove n-th line when "small" is NULL

--- srca/ft_matrix_replace_in.c
+++ srca/ft_matrix_replace_in.c
@@ -6,7 +6,7 @@
 /*   By: aperez-b <aperez-b@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/11 18:28:15 by aperez-b          #+#    #+#             */
-/*   Updated: 2021/11/12 00:15:24 by aperez-b         ###   ########.fr       */
+/*   Updated: 2021/11/12 07:39:02 by aperez-b         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,7 +20,7 @@ char	**ft_matrix_replace_in(char ***big, char **small, int n)
 	i[0] = -1;
 	i[1] = -1;
 	i[2] = -1;
-	if (!big || !*big || !small || n < 0 || n >= ft_matrixlen(*big))
+	if (!big || !*big || n < 0 || n >= ft_matrixlen(*big))
 		return (NULL);
 	aux = ft_calloc(ft_matrixlen(*big) + ft_matrixlen(small), sizeof(char *));
 	while (aux && big[0][++i[0]])
@@ -29,7 +29,7 @@ char	**ft_matrix_replace_in(char ***big, char **small, int n)
 			aux[++i[2]] = ft_strdup(big[0][i[0]]);
 		else
 		{
-			while (small[++i[1]])
+			while (small && small[++i[1]])
 				aux[++i[2]] = ft_strdup(small[i[1]]);
 		}
 	}
